bc798a55de6c6eba193aea59aced81d2ea3d7b56
Michael Elrod <michaelelrod.dev@gmail.com>
2023-03-29 19:07:36 -0400

    Asset Cleanup

--- Src/Settings.py
+++ Src/Settings.py
@@ -7,11 +7,7 @@ screen_width = 1280
 level_0 = {
     'terrain': 'Assets/levels/0/level_0_terrain.csv',
     'coins':'Assets/levels/0/level_0_coins.csv',
-    'fg palms':'Assets/levels/0/level_0_fg_palms.csv',
-    'bg palms':'Assets/levels/0/level_0_bg_palms.csv',
-    'crates': 'Assets/levels/0/level_0_crates.csv',
     'enemies':'Assets/levels/0/level_0_enemies.csv',
     'constraints':'Assets/levels/0/level_0_constraints.csv',
     'player': 'Assets/levels/0/level_0_player.csv',
-    'grass': 'Assets/levels/0/level_0_grass.csv'
 }
